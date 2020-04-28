087e5db1a618c1b76a435148ac59c0f1185ced0b
matthewsedlacek <sedlacek1991@gmail.com>
2020-04-28 11:57:38 -0700

    Finitodos.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,55 @@
+require 'pry'
+
 class Customer
   attr_accessor :given_name, :family_name
+
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @full_name = "#{@given_name} #{@family_name}"
+    @@all << self
   end
 
   def full_name
-    "#{given_name} #{family_name}"
+    @@all.each do |customer|
+      puts "#{customer.given_name} #{customer.family_name}"
+    end
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.select {|reviews|review.restaurant == self}
+  end
+
+  def self.find_by_name (full_name)
+    Review.all.find {|review|review.customer == self.full_name}
+  end
+
+  def self.find_all_by_given_name(name)
+    Review.all.select {|review| review.customer == self.given_name}
+  end 
+
+  def all_ratings
+]    Review.all.map {|reviews| reviews.rating}
+  end
+
+  def num_reviews
+    all_ratings.length
+  end
+
+  def average_star_rating
+    all_ratings[0] / num_reviews
+  end
+
+  def add_review(restaurant,rating)
+    Review.new(self,restaurant,rating)
+  end
+
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
 class Review
-  
+  attr_accessor :rating
+  attr_reader :customer, :restaurant
+
+  @@all = []
+
+  def initialize(customer, restaurant, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+
+    @@all <<self
+
+  end
+
+  def rating
+    @rating
+  end
+
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+  def customer
+    Customer.all.select {|customer|customer.given_name == self.customer}
+  end
+
+  def restaurant
+    Restaurant.all.select {|restaurant|restaurant.name == self.restaurant}
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end

18bc3f4747e6185a8d6e0bd76d21e1eef83e4ff9
github username <github email address>
2020-04-28 10:20:44 -0700

    Andy del valle code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,16 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    ratings = self.reviews.map {|review| review.rating}
+    ratings.sum/ratings.count
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("given_name1", "family_name1")
+c2 = Customer.new("given_name2", "family_name2")
+c3 = Customer.new("given_name1", "family_name3")
+
+r1 = Restaurant.new("restaurant1")
+r2 = Restaurant.new("restaurant2")
+
+review1 = Review.new(c1, r1, 4)
+review2 = Review.new(c2, r1, 5)
+review3 = Review.new(c3, r2, 4)
+review4 = Review.new(c2, r1, 3)
+review5 = Review.new(c3, r2, 3)
+
+review6 = c1.add_review(r1,3)
+review7 = c1.add_review(r2,5)
+
+
+puts "you got this"
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line

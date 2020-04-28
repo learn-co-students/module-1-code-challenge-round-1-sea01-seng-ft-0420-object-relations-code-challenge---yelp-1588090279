4fd7c5648ce08e20fcd630d25cc55a7f3ccb6d98
NicoleQiu99 <nicolepeng99@gmail.com>
2020-04-28 11:01:57 -0700

    1 version

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_reader :name
-
+  
   def initialize(name)
     @name = name
+  end
+  #returns an array of all reviews for that restaurant
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+  #Returns a unique list of all customers who have reviewed a particular restaurant.
+  def customers
+    self.reviews.coustomer
+  end
+
+  #returns the average star rating for a restaurant based on its reviews
+  def average_star_rating
+    rate = []
+    Review.all.restaurant.select do |res|
+      res == self
+      rate << Review.all.rating
+    end
+    sum = 0
+    rate.each {|r| sum += r} 
+    sum/rate.length
+    
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,22 @@
 class Review
-  
+    attr_reader :customer, :restaurant
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+    
+    #returns the rating for a restaurant.
+    def rating
+        @rating
+    end
+    
+    #returns all of the reviews
+    def self.all
+        @@all
+    end
+
 end

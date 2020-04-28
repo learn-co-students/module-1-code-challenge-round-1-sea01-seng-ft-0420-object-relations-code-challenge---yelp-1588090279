3ff8e929432ce599e2401ec8cbec4e105b6ac7ee
johnrusch <johnrusch7@gmail.com>
2020-04-28 10:01:46 -0700

    John Rusch code challenge 1 solution

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,20 @@ class Restaurant
     @name = name
   end
 
+  # returns an array of all reviews for that restaurant
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  # returns a unique list of all customers who have reviewed a particular restaurant
+  def customers
+    self.reviews.map {|review| review.customer}.uniq
+  end
+
+  # returns the average star rating for a restaurant based on its reviews
+  def average_star_rating
+    ratings = self.reviews.map {|review| review.rating}
+    ratings.sum(0.0) / ratings.size
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,22 @@
 class Review
-  
+    attr_reader :customer, :restaurant, :rating
+
+    @@all = []
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    private
+
+    def save
+        @@all << self
+    end
+
 end

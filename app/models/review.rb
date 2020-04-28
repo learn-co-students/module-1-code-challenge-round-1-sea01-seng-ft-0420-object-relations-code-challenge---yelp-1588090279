60e93d9fbf863d73d915cc8a09ded2ab3dfb4652
ErinReames <erinreames@icloud.com>
2020-04-28 10:58:02 -0700

    Message

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,28 @@
+require "pry"
 class Review
+    attr_accessor :rating
+    attr_reader :customer, :restaurant
   
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating 
+
+        save 
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
+    
 end
+
+#erin = Review.new("Erin", "Dragon City", 5)
+#binding.pry 
